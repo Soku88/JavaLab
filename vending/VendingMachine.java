97518c4a12094a98d58b22813687ea7935c92b19
Soku88 <essence.soku@gmail.com>
2020-12-09 01:23:51 +0900

    Initial commit

---
+++ vending/VendingMachine.java
@@ -0,0 +1,66 @@
+package com.koreait.first.vending;
+
+import java.util.Scanner;
+
+public class VendingMachine {
+	private Scanner sc;		//Member field에는 선언만 해준다.
+	private Can[] canArr;
+	private int sum;
+	
+	public VendingMachine() { //main에 init()메소드 전달
+		init();
+	}
+	private void init() {
+		String[] nmArr = {"콜라", "사이다", "환타", "솔의눈", "데자와"};
+		int[] priceArr = {500, 600, 700, 800, 900};
+		sc = new Scanner(System.in);
+		
+		canArr = new Can[nmArr.length]; //canArr에 새로운 주소값 생성
+		
+		for(int i=0; i<canArr.length; i++) {
+			canArr[i] = new Can(nmArr[i], priceArr[i]); //canArr[i] > nmArr, priceArr 생성해주는 반복문
+		}
+	}
+	public void showMenus() { //Can의 주소값을 canArr이 이미 알고있으므로 참조가능
+		System.out.println("----메뉴----");
+		for (int i = 0; i < canArr.length; i++) {
+			System.out.printf("%d. %s - (%,d원)\n", i + 1, canArr[i].getNm(),canArr[i].getPrice());
+		}
+	}
+//	public void purchase() {
+//		int menu = 0;
+//		while(true) {
+//			System.out.printf("메뉴 입력 (1~4, 종료 : 0) : ");
+//			menu = sc.nextInt();		
+//			if(menu == 0) { // break;
+//				break;
+//			}
+//			if(menu > canArr.length || menu < 0) { // menu ~ priceArr.length까지
+//				System.out.println("잘못 입력하셨습니다. 다시입력 해 주세요");	
+//				continue; // menu ~ priceArr.length가 아니라면 계속진행
+//			}
+//			sum += canArr[menu - 1].getPrice();	
+//			}
+//		System.out.println(sum);
+//		sc.close();
+//		}
+	
+	public void purchase() {
+		int menu = 0;
+		while(true) {
+			System.out.print("메뉴 입력 (종료 : 0) : ");
+			menu = sc.nextInt();
+			if(menu == 0) {
+				break;
+			} else if (menu > canArr.length || menu < 0) {
+				System.out.println("잘못 입력하셨습니다. 다시입력해 주세요");
+			} else {
+				sum += canArr[menu-1].getPrice(); //canArr[menu-1] < 배열의 주소값 이므로
+												  //int형(.getPrice)으로 sum연산
+			}
+		}
+		System.out.println(sum); //if문 밖에서 실행
+		sc.close(); //자원정리//
+	}	
+}
+
---
+++ vending/privateTest.java
@@ -0,0 +1,16 @@
+package com.koreait.first.vending;
+
+public class privateTest {
+
+	public static void main(String[] args) {
+		privateTest2 pT2 = new privateTest2(); // pT2에 privateTest2 주소값 생성
+		pT2.setName("mee"); //pT2.로 privateTest2의 멤버에 접근가능
+		pT2.setAge(30);
+		
+		System.out.println(pT2.getName());
+		System.out.println(pT2.getAge());
+		
+		
+	}
+
+}
